78baf7916b87cc9b002de8d23fbd83c0a6a4be2f
James Brady <jfbrady12345@gmail.com>
2021-09-26 18:57:46 +1300

    Implemented dynamically searchable shop.

--- jbra654.js
+++ jbra654.js
@@ -23,6 +23,7 @@ const showInstituteShop = () => {
     document.getElementById("userRegistration").style.display = "none";
     document.getElementById("guestBook").style.display = "none";
     document.title = "Institute Shop";
+    getProducts();
 }
 
 const showUserRegistration = () => {
@@ -43,14 +44,51 @@ const showGuestBook = () => {
     document.title = "Guest Book";
 }
 
-window.onload = showHome;
-
 const getVersion = () => {
     const fetchPromise = fetch('http://localhost:5000/api/GetVersion');
     const streamPromise = fetchPromise.then((response) => response.text());
     streamPromise.then((data) => { document.getElementById('version').innerText = data; });
 }
 
+const getProducts = () => {
+    const fetchPromise = fetch('http://localhost:5000/api/GetItems');
+    const streamPromise = fetchPromise.then((response) => response.json());
+    streamPromise.then((data) => showDetails(data));
+}
+
+const showDetails = (products) => {
+    let htmlString = "<tr class='title'><td>ID</td><td>Image</td><td>Name</td><td>Description</td><td>Price</td></tr>";
+
+    const showProduct = (item) => {
+        htmlString += `<tr><td>${item.id}</td><td><img src="http://localhost:5000/api/GetItemPhoto/${item.id}" alt="${item.name} image" height="100"></td><td>${item.name}</td><td>${item.description}</td><td>$${item.price}</td></tr>`
+    }
+    products.forEach(showProduct)
+
+    const productsTable = document.getElementById("productsTable");
+    productsTable.innerHTML = htmlString;
+}
+
+function productSearch() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("productsTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+window.onload = showHome;
 getVersion();
 
 
